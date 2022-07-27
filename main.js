0b60903cc08cd7b4c514649a886932efe16d0af5
kessio <64524758+kessio@users.noreply.github.com>
2022-07-27 16:12:08 +0300

    Fixed eslint error

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-/* =========== Menu poup Start Here ==================== */
 const menuNav = document.querySelector('.mobile-nav-list');
 const hamburger = document.querySelector('.mobile-menu-hamburger');
 const menuItems = document.querySelectorAll('.mobile-nav-item');
 const closemenu = document.querySelector('.close-menu');
 hamburger.addEventListener('click', () => {
   menuNav.classList.toggle('active');
-}); 
+});
 closemenu.addEventListener('click', () => { menuNav.classList.remove('active'); });
 menuItems.forEach((n) => n.addEventListener('click', () => {
   menuNav.classList.remove('active');
